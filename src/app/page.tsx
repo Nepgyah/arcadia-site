8f4edc46cf7455f25a5991b543d1d5c58773fcd1
Auston <nepgyah@gmail.com>
2026-03-11 14:06:57 -0500

    feat: add base layout files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+import '@/styles/pages/_home.scss';
+import { Button } from '@chakra-ui/react';
+
 export default function Page() {
   return (
     <div id="page-home">
-      Home
+      
+      <div id="hero">
+        <img className='bg-element' src="/pages/home/hero-background.png" alt="" />
+        <div className="wrapper">
+          <div>
+            <div className='hero-text'>
+              <h1>Your Anime <br />Your Way</h1>
+              <p>Arcadia is your home base for everything otaku. Where fans come together to celebrate what they love and find something new.</p>
+              <div className="cta-container">
+                <Button className='primary'>
+                  Explore Arcadia
+                </Button>
+              </div>
+              <div id='main-points'></div>
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </div>
     </div>
   )
 }
