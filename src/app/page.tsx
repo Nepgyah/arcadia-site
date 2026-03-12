89595d8288b49983c0846a2d3efef76bfed89b34
Auston <nepgyah@gmail.com>
2026-03-12 01:41:37 -0500

    fix: section bg gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,145 +38,145 @@ export default function Page() {
         </div>
       </div>
       
-      <section id='catchphrase' className='clr-txt-light'>
-        <div className='wrapper'>
-          <div id='part-one'>
-            <div>
-              <FadeIn direction='right'>
-                <img src="/pages/home/catchphrase-one.png" alt="" />
-              </FadeIn>
-            </div>
-            <div className='center-v'>
-              <h2>Your Otaku</h2>
-            </div>
-          </div>
-          <div id='part-two'>
-            <div className='center-b'>
-              <h2 className='clr-arc-highlight'>Sanctuary</h2>
-            </div>
-            <div>
-              <FadeIn direction='left'>
-                <img src="/pages/home/catchphrase-two.png" alt="" />
-              </FadeIn>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section id='sponsor'>
-        <img id='broken-circle' className='element' src="/assets/broken-circle.svg" alt="" />
-        <div className='wrapper'>
-          <div>
-            <div id='sponsor-container'>
-              <div className='sponsor-card'>
-                <img src="/sponsors/eagle-jump.png" alt="" />
-              </div>
-              <div className='sponsor-card'>
-                <img src="/sponsors/astral-express.png" alt="" />
-              </div>
-              <div className='sponsor-card'>
-                <img src="/sponsors/aniplex-white.svg" alt="" />
-              </div>
-
-              <div className='sponsor-card'>
-                <img src="/sponsors/fromsoft.png" alt="" />
-              </div>
-              <div className='sponsor-card'>
-                <img src="/sponsors/hoyoverse-white.png" alt="" />
-              </div>
-              <div className='sponsor-card'>
-                <img src="/sponsors/jc-staff.png" alt="" />
-              </div>
-
-              <div className='sponsor-card'>
-                <img src="/sponsors/myanimelist.svg" alt="" />
-              </div>
-              <div className='sponsor-card'>
-                <img src="/sponsors/nexon-white.png" alt="" />
-              </div>
-              <div className='sponsor-card'>
-                <img src="/sponsors/yostar-white.svg" alt="" />
-              </div>
-            </div>
-            <div className='center-v'>
-              <div className='text text--white'>
-                <p className='mini-title'>Arcadia Partners</p>
-                <h2>Industry Leaders <span className='clr-arc-highlight'>Collab</span> With Arcadia</h2>
-                <p>Anime Studios, Game Developers, and more collaborate with Arcadia to build something special. </p>
-                <p><b>Names used here are purely fictional for Arcadia/D2X world building. These companies do not sponsor or endorse Arcadia.</b></p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section id='primary-apps' className='bg-arc-base'>
-        <div className='wrapper'>
-          <div>
-            <div id='top-half'>
-              <div className='text text--white'>
-                <p className='mini-title'>Arcadia Apps</p>
-                <h2>Essentials for the <span className='clr-arc-highlight'>Ultimate</span> Otaku</h2>
-                <p>Your all-in-one toolkit for tracking, streaming, gaming, and shopping. These flagship apps form the heart of your ultimate otaku experience.</p>
-              </div>
-              <FadeIn direction='left'>
-                <div id='miru-card' className='app-card bg-miru-base clr-txt-dark p-a-md shadow border-radius-md'>
-                  <h3>Miru - みる [見る]</h3>
-                  <p>The Ultimate Anime App</p>
-                  <img src="/pages/home/apps/miru-mascots.png" alt="" />
-                  <div className='circle bg-miru-dark'></div>
-                </div>
-              </FadeIn>
-            </div>
-
-            <div id='bottom-half'>
-
-              <div id='left-half'>
+      <div className='bg-gradient'>
+        <section id='catchphrase' className='clr-txt-light'>
+          <div className='wrapper'>
+            <div id='part-one'>
+              <div>
                 <FadeIn direction='right'>
-                  <div id='yomu-card' className='app-card bg-yomu-base clr-txt-dark p-a-md shadow border-radius-md'>
-                    <h3>Yomu - よる [読む]</h3>
-                    <p>Stories that stay with you</p>
-                    <div className='circle'></div>
-                    <img src="/pages/home/apps/yomu-mascots.png" alt="" />
-                    <div className='circle bg-yomu-dark'></div>
-                  </div>
-                </FadeIn>
-                <FadeIn direction='right' delay={.3}>
-                  <div id='asobu-card' className='app-card bg-asobu-base clr-txt-light p-a-md shadow border-radius-md'>
-                    <h3>Asobu - あそぶ [遊ぶ]</h3>
-                    <p>Gaming adventures begin here</p>
-                    <div className='circle'></div>
-                    <img src="/pages/home/apps/asobu-mascots.png" alt="" />
-                    <div className='circle bg-asobu-dark'></div>
-                  </div>
+                  <img src="/pages/home/catchphrase-one.png" alt="" />
                 </FadeIn>
               </div>
-
-              <div id='right-half'>
+              <div className='center-v'>
+                <h2>Your Otaku</h2>
+              </div>
+            </div>
+            <div id='part-two'>
+              <div className='center-b'>
+                <h2 className='clr-arc-highlight'>Sanctuary</h2>
+              </div>
+              <div>
                 <FadeIn direction='left'>
-                  <div id='kiku-card' className='app-card bg-kiku-base clr-txt-dark p-a-md shadow border-radius-md'>
-                    <h3>Kiku - きく [聞く]</h3>
-                    <p>Gaming adventures begin here</p>
-                    <div className='circle'></div>
-                    <img src="/pages/home/apps/kiku-mascots.png" alt="" />
-                    <div className='circle bg-kiku-dark'></div>
-                  </div>            
-                </FadeIn>
-                <FadeIn direction='left' delay={.4}>
-                  <div id='iku-card' className='app-card bg-iku-base clr-txt-light p-a-md shadow border-radius-md'>
-                    <h3>Iku - いく [行く]</h3>
-                    <p>Make every meetup a adventure</p>
-                    <div className='circle'></div>
-                    <img src="/pages/home/apps/iku-mascots.png" alt="" />
-                    <div className='circle bg-iku-dark'></div>
-                  </div>
+                  <img src="/pages/home/catchphrase-two.png" alt="" />
                 </FadeIn>
               </div>
-
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+        <section id='sponsor'>
+          <img id='broken-circle' className='element' src="/assets/broken-circle.svg" alt="" />
+          <div className='wrapper'>
+            <div>
+              <div id='sponsor-container'>
+                <div className='sponsor-card'>
+                  <img src="/sponsors/eagle-jump.png" alt="" />
+                </div>
+                <div className='sponsor-card'>
+                  <img src="/sponsors/astral-express.png" alt="" />
+                </div>
+                <div className='sponsor-card'>
+                  <img src="/sponsors/aniplex-white.svg" alt="" />
+                </div>
+
+                <div className='sponsor-card'>
+                  <img src="/sponsors/fromsoft.png" alt="" />
+                </div>
+                <div className='sponsor-card'>
+                  <img src="/sponsors/hoyoverse-white.png" alt="" />
+                </div>
+                <div className='sponsor-card'>
+                  <img src="/sponsors/jc-staff.png" alt="" />
+                </div>
+
+                <div className='sponsor-card'>
+                  <img src="/sponsors/myanimelist.svg" alt="" />
+                </div>
+                <div className='sponsor-card'>
+                  <img src="/sponsors/nexon-white.png" alt="" />
+                </div>
+                <div className='sponsor-card'>
+                  <img src="/sponsors/yostar-white.svg" alt="" />
+                </div>
+              </div>
+              <div className='center-v'>
+                <div className='text text--white'>
+                  <p className='mini-title'>Arcadia Partners</p>
+                  <h2>Industry Leaders <span className='clr-arc-highlight'>Collab</span> With Arcadia</h2>
+                  <p>Anime Studios, Game Developers, and more collaborate with Arcadia to build something special. </p>
+                  <p><b>Names used here are purely fictional for Arcadia/D2X world building. These companies do not sponsor or endorse Arcadia.</b></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id='primary-apps'>
+          <div className='wrapper'>
+            <div>
+              <div id='top-half'>
+                <div className='text text--white'>
+                  <p className='mini-title'>Arcadia Apps</p>
+                  <h2>Essentials for the <span className='clr-arc-highlight'>Ultimate</span> Otaku</h2>
+                  <p>Your all-in-one toolkit for tracking, streaming, gaming, and shopping. These flagship apps form the heart of your ultimate otaku experience.</p>
+                </div>
+                <FadeIn direction='left'>
+                  <div id='miru-card' className='app-card bg-miru-base clr-txt-dark p-a-md shadow border-radius-md'>
+                    <h3>Miru - みる [見る]</h3>
+                    <p>The Ultimate Anime App</p>
+                    <img src="/pages/home/apps/miru-mascots.png" alt="" />
+                    <div className='circle bg-miru-dark'></div>
+                  </div>
+                </FadeIn>
+              </div>
+
+              <div id='bottom-half'>
+
+                <div id='left-half'>
+                  <FadeIn direction='right'>
+                    <div id='yomu-card' className='app-card bg-yomu-base clr-txt-dark p-a-md shadow border-radius-md'>
+                      <h3>Yomu - よる [読む]</h3>
+                      <p>Stories that stay with you</p>
+                      <div className='circle'></div>
+                      <img src="/pages/home/apps/yomu-mascots.png" alt="" />
+                      <div className='circle bg-yomu-dark'></div>
+                    </div>
+                  </FadeIn>
+                  <FadeIn direction='right' delay={.3}>
+                    <div id='asobu-card' className='app-card bg-asobu-base clr-txt-light p-a-md shadow border-radius-md'>
+                      <h3>Asobu - あそぶ [遊ぶ]</h3>
+                      <p>Gaming adventures begin here</p>
+                      <div className='circle'></div>
+                      <img src="/pages/home/apps/asobu-mascots.png" alt="" />
+                      <div className='circle bg-asobu-dark'></div>
+                    </div>
+                  </FadeIn>
+                </div>
+
+                <div id='right-half'>
+                  <FadeIn direction='left'>
+                    <div id='kiku-card' className='app-card bg-kiku-base clr-txt-dark p-a-md shadow border-radius-md'>
+                      <h3>Kiku - きく [聞く]</h3>
+                      <p>Gaming adventures begin here</p>
+                      <div className='circle'></div>
+                      <img src="/pages/home/apps/kiku-mascots.png" alt="" />
+                      <div className='circle bg-kiku-dark'></div>
+                    </div>            
+                  </FadeIn>
+                  <FadeIn direction='left' delay={.4}>
+                    <div id='iku-card' className='app-card bg-iku-base clr-txt-light p-a-md shadow border-radius-md'>
+                      <h3>Iku - いく [行く]</h3>
+                      <p>Make every meetup a adventure</p>
+                      <div className='circle'></div>
+                      <img src="/pages/home/apps/iku-mascots.png" alt="" />
+                      <div className='circle bg-iku-dark'></div>
+                    </div>
+                  </FadeIn>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
 
       <section id='stack'>
         <div className='wrapper'>
